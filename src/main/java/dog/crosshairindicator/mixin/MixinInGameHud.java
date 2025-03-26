12a6c27a540bd7da96e968ee2be84df0c55684b2
noryea <864460295@qq.com>
2025-03-26 16:46:42 +0800

    Fix indicator translucent visual effect & not working for mobs.

--- src/main/java/dog/crosshairindicator/mixin/MixinInGameHud.java
+++ src/main/java/dog/crosshairindicator/mixin/MixinInGameHud.java
@@ -4,7 +4,6 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.RenderTickCounter;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Final;
@@ -21,16 +20,15 @@ public class MixinInGameHud {
 	@Shadow @Final private MinecraftClient client;
 	@Unique Identifier textureLocation = Identifier.of("crosshairindicator", "textures/gui/hud/crosshair.png");
 
-	@Inject(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V"))
+	@Inject(method = "renderCrosshair", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Ljava/util/function/Function;Lnet/minecraft/util/Identifier;IIII)V", ordinal = 0))
 	private void drawCrosshair(DrawContext context, RenderTickCounter tickCounter, CallbackInfo ci) {
-		if (this.client.targetedEntity instanceof PlayerEntity) {
-			RenderSystem.setShaderTexture(0, textureLocation);
-			RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-			int scaledWidth = 15;
-			int scaledHeight = 15;
+        if (this.client.targetedEntity != null && this.client.targetedEntity.isAlive()) {
+            RenderSystem.setShaderTexture(0, textureLocation);
+            int scaledWidth = 15;
+            int scaledHeight = 15;
 
-			context.drawTexture(RenderLayer::getGuiTextured, textureLocation, (context.getScaledWindowWidth() - scaledWidth) / 2, (context.getScaledWindowHeight() - scaledHeight) / 2, 0.0F, 0.0F, scaledWidth, scaledHeight, scaledWidth, scaledHeight);
-		}
-	}
+            context.drawTexture(RenderLayer::getCrosshair, textureLocation, (context.getScaledWindowWidth() - scaledWidth) / 2, (context.getScaledWindowHeight() - scaledHeight) / 2, 0.0F, 0.0F, scaledWidth, scaledHeight, scaledWidth, scaledHeight);
+        }
+    }
 
 }
